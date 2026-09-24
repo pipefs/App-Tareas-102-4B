6e7b9abe06b4c5f8a51bd7abdfa0297655233e2b
Talleres Puente Alto <tallerespa@inacapmail.cl>
2026-09-24 14:05:48 -0300

    Fin clase

--- tareasapp/urls.py
+++ tareasapp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name= 'inicio'),
+    path('crear_tarea', views.crear_tarea, name= 'crear_tarea'),
+    path('ver_tarea/<int:tid>/', views.ver_tarea, name= 'ver_tarea'),
+    path('editar_tarea/<int:tid>/', views.editar_tarea, name= 'editar_tarea'),
+    path('eliminar_tarea/<int:tid>/', views.eliminar_tarea, name= 'eliminar_tarea'),
 ]
--- tareasapp/views.py
+++ tareasapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Tarea
 # Create your views here.
@@ -12,3 +12,46 @@ def inicio(request):
     return render(request, 'tareasapp/inicio.html', {
         'tareas': tareas
     }) 
+
+def crear_tarea(request):
+    if request.method == "POST":
+        titulo = request.POST['titulo']
+        descripcion = request.POST['descripcion']
+        completada = 'completada' in request.POST
+
+        Tarea.objects.create(
+            titulo = titulo,
+            descripcion = descripcion,
+            completada = completada
+        )
+
+        return redirect('inicio')
+
+    return render(request, 'tareasapp/crear.html')
+
+def ver_tarea(request, tid):
+    tarea = Tarea.objects.get(id=tid)
+
+    return render(request, 'tareasapp/detalle.html', {'tarea': tarea}) 
+
+def editar_tarea(request, tid):
+    tarea = Tarea.objects.get(id=tid)
+
+    if request.method == "POST":
+        tarea.titulo = request.POST['titulo']
+        tarea.descripcion = request.POST['descripcion']
+        tarea.completada = 'completada' in request.POST
+        tarea.save()
+        
+        return redirect('inicio')
+
+    return render(request, 'tareasapp/editar.html', {'tarea': tarea}) 
+
+def eliminar_tarea(request, tid):
+    tarea = Tarea.objects.get(id=tid)
+
+    if request.method == "POST":
+        tarea.delete()
+        return redirect('inicio')
+
+    return render(request, 'tareasapp/eliminar.html') 
